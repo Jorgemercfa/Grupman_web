dec1380fc23a46acb3d953da38c438dad598896e
Jorgemercfa <cokijlmf@outlook.com>
2026-02-15 17:03:59 -0500

    Update navbar-area class with new styles

--- src/components/Navbar-item.vue
+++ src/components/Navbar-item.vue
@@ -1,43 +1,18 @@
 <template>
   <div class="navbar-area">
-    <img class="logo" src="@/assets/company-logo.jpg" />
-    <div class="link-style">
-      <router-link to="/" class="text-navbar">Inicio</router-link>
-      <router-link to="/About-item" class="text-navbar">Nosotros</router-link>
-      <router-link to="/Services-item" class="text-navbar"
-        >Servicios</router-link
-      >
-      <router-link to="/Contact-item" class="text-navbar">Contacto</router-link>
-    </div>
+    <!-- Your existing code -->
   </div>
 </template>
-
-<script></script>
-
-<style>
+<script>
+export default {
+  name: 'NavbarItem',
+};
+</script>
+<style scoped>
 .navbar-area {
-  height: 70px;
-  background-color: #42ae1a;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 30px;
-}
-
-.link-style {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 30px;
-}
-
-.logo {
-  width: 50px;
-  height: 50px;
-}
-
-.text-navbar {
-  color: white;
-  text-decoration: none;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  flex-shrink: 0;
 }
 </style>
